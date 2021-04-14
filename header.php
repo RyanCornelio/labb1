4824f5ca47920085819e3fbbba89982b00d9540d
Ryan-Phillips Cornelio <ryan.cornelio@gmail.com>
2021-04-14 02:17:00 +0200

    Parted header and footer

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Labb 1</title>
+	<link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="<?= get_template_directory_uri().'/bootstrap.css' ?>">
+	<link rel="stylesheet" href="<?= get_template_directory_uri().'/style.css' ?>">
+	<script src="js/jquery.js"></script>
+</head>
